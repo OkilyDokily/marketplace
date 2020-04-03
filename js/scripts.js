e3d5b9d013e1bc79ac1d0646e1941da9dc632699
me <me@me.me.com>
2020-04-03 14:57:17 -0700

    write jquery for modal

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,17 @@ $(document).ready(function(){
 
   $("#formOne").submit(function(e){
     e.preventDefault();
+
+
+    $(".firstname").text($("#firstname").val());
+    $(".lastname").text($("#lastname").val());
+    $(".address").text($("#address").val());
+    $(".apt").text($("#apt").val());
+    $(".city").text($("#city").val());
+    $(".state").text($("#state").val());
+    $(".zip").text($("#zip").val());
     $(".modal").show();
+
   });
 
 })
