68eaa9a57d67f1313044a40c3842f1209652c5fd
me <me@me.me.com>
2020-04-03 15:38:56 -0700

    make minor fix on modal

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,14 @@ $(document).ready(function(){
   $("#formOne").submit(function(e){
     e.preventDefault();
 
-
+    var amount = parseInt($("#amount").val());
+    var total = 55.99 * amount;
+    $(".total").text(total);
     $(".firstname").text($("#firstname").val());
+
     $(".lastname").text($("#lastname").val());
     $(".address").text($("#address").val());
-    $(".apt").text($("#apt").val());
+    $(".apt").text("Apt " + $("#apt").val());
     $(".city").text($("#city").val());
     $(".state").text($("#state").val());
     $(".zip").text($("#zip").val());
