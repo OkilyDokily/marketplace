9622828554fc1564c55527c7471b706dc8161cfa
me <me@me.me.com>
2020-04-07 01:07:58 -0700

    refactor jquery to use an array

--- js/scripts.js
+++ js/scripts.js
@@ -9,26 +9,27 @@ $(document).ready(function(){
     var amountOfToiletPaper = parseInt($("#amount").val());
     
     $(".total").text(totalCost(amountOfToiletPaper));
-    $(".firstname").text($("#firstname").val());
-
-    $(".lastname").text($("#lastname").val());
-    $(".address").text($("#address").val());
-    $(".apt").text("Apt " + $("#apt").val());
-    $(".city").text($("#city").val());
-    $(".state").text($("#state").val());
-    $(".zip").text($("#zip").val());
-    $(".modal").show();
     
+    var formItem = ["firstname","lastname","address","apt","city","state","zip"];
+    
+    formItem.forEach(function(item){
+      $("."+item).text($("#"+item).val())
+    });
+    
+    
+    $(".modal").show();
     $(".modal").animate({opacity:1},1000);
     
+    
+
       
-     
-   
     $("button").prop("disabled", true);
     $('#formOne').each(function(){
       this.reset();
     });
   });
+
+  
 
  
 
